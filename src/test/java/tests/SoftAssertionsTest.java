200d9d06a8c3b302f7906dbd52d340cb7be0a3f4
AndreyLutovinov <andreilute@gmail.com>
2024-12-07 12:58:23 +0300

    added checks-1

--- src/test/java/tests/SoftAssertionsTest.java
+++ src/test/java/tests/SoftAssertionsTest.java
@@ -1,12 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,37 +16,29 @@ public class SoftAssertionsTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
-    }
-
-    @AfterEach
-    void afterEachTest() {
-        Selenide.closeWebDriver();
+        Configuration.timeout = 4000;
+        Configuration.holdBrowserOpen = true;
     }
 
 
     @Test
-    void gitHubSelenideTest() {
-        // открыть страницу Selenide в Github
+    void HubSelenideTest() {
         open("/selenide/selenide");
-           //переход, поиск и проверка в wiki
-              $("#wiki-tab").click();
-              $("#wiki-pages-filter").setValue("SoftAssertions");
-              $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-              $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-              //проверка кода
-        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class}) \n" +
-                        "class Tests { \n" +
-                        "@Test \n" +
-                        "void test() { \n" +
-                        "Configuration.assertionMode = SOFT; \n" +
-                        "open(\"page.html\"); \n" +
-                        " \n" +
-                        "$(\"#first\").should(visible).click(); \n" +
-                        "$(\"#second\").should(visible).click(); \n" +
-                        "} \n" +
-                        "} \n"));
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".filterable-active").shouldHave(text("SoftAssertions"));
+        $(".filterable-active").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
     }
-
-
 }
